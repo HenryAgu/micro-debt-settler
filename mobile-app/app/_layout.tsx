400e15b618e81cf72dd4c80221cd97ea98bd8661
Henry Agu <aguhenrychuks@gmail.com>
2025-11-10 11:34:06 +0100

    feat: onboarding and sign up screens

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,18 +1,26 @@
-import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+// app/_layout.tsx
 import { Stack } from "expo-router";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { ThemeProvider, DefaultTheme } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import "../global.css";
 
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
-
 export default function RootLayout() {
+    const [fontsLoaded] = useFonts({
+    RobotoRegular: Roboto_400Regular,
+    RobotoMedium: Roboto_500Medium,
+    RobotoBold: Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <ThemeProvider value={DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
